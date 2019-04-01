e5c23bca5be5ab8d283a508715513bcd5ab06160
Mark Meretzky <mark.meretzky@gmail.com>
2019-04-01 15:54:02 -0400

    Update ViewController.swift

--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -16,13 +16,11 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad();
+
         // Do any additional setup after loading the view.
         if let todo: Todo = todo {
             nameTextField.text = todo.name;
             descriptionTextField.text = todo.description;
         }
     }
-
-
 }
-
