1006866bfdd46794f257baa3b7d27c4311123e73
Mark Meretzky <mark.meretzky@gmail.com>
2019-04-02 08:14:58 -0400

    Update ViewController.swift

--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -14,13 +14,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameTextField: UITextField!;
     @IBOutlet weak var descriptionTextField: UITextField!;
     
+    // Arrive here when user presses the + button or taps a cell in the TodoTableViewController.
+    
     override func viewDidLoad() {
         super.viewDidLoad();
 
         // Do any additional setup after loading the view.
         if let todo: Todo = todo {
+            viewController.navigationItem.title = "Update To Do";
             nameTextField.text = todo.name;
             descriptionTextField.text = todo.description;
+        } else {
+            viewController.navigationItem.title = "New To Do";
         }
     }
+    
+    // Arrive here when user presses Save or Cancel.
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+ 
+        guard segue.identifier == "SaveSegue" else {
+            return;  //User pressed Cancel, so do nothing.
+        }
+        
+        todo = Todo(name: nameTextField.text ?? "", description: descriptionTextField ?? "");
+    }
+
 }
